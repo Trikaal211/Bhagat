b7e93c1740b045f171baf5ed46ad93c68f0711e0
Trikaal212 <anikets993@gmail.com>
2025-07-23 17:34:07 +0530

    set base

--- Freedom/vite.config.js
+++ Freedom/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/apna/',   // <-- ye zaruri hai (repo name ke hisaab se)
+  base: '/bhagat/',   // <-- ye zaruri hai (repo name ke hisaab se)
 })
