e9b47f569e138d18fe64c623c35e3a0020f50c87
Trikaal212 <anikets993@gmail.com>
2025-07-23 17:38:19 +0530

    repo name

--- Freedom/vite.config.js
+++ Freedom/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bhagat/',   // <-- ye zaruri hai (repo name ke hisaab se)
+  base: '/Bhagat/'
 })
